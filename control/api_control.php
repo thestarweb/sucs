e2a168bc4f8bca21bde97c11bdf7ef8af61948c8
thestarweb <597577939@qq.com>
2016-01-14 16:04:00 +0800

    安全提升，app访问需要密码
    注：为保证兼容性，原api暂未封锁，过渡期后将关闭！！请及时更新

--- control/api_control.php
+++ control/api_control.php
@@ -1,30 +1,18 @@
 <?php
 class api_control{
 	public function __construct($system){
-		//$this->app=app_server($system);
+		$this->app=new app_server($system);
 		//if(!empty($_GET['app_id'])||!$this->app->star($_GET['app_id'],$_GET['time'])) $system->show_json(array('error'=>'无效的应用'));
-		header('Access-Control-Allow-Origin: http://blog.thestarweb.sweb');
-		header('Access-Control-Allow-Credentials:true');
-	}
-	/*public function get_key_page($system){
-		if(true){
-			die('非法访客');
-		}
-		$login=login_server($system);
-		if($login->is_login===false){
-			$system->show_json(array('info'=>''));
-		}else{
-			$system->show_json(array('info'=>array('key'=>$_COOKIE['r_login_key'],'id'=>$_COOKIE['r_login_key'])));
-		}
 	}
 	public function is_login_page($system){
-		if($this->app->is_user($_GET['login_key'],$_GET['key'])){
-			$system->show_json(array('is_login'=>1));
+		if($this->app->is_admit()){
+			$login=new login_server($system);
+			$system->show_json(array('u'=>$login->get_now_user()));
 		}else{
-			$system->show_json(array('is_login'=>0));
+			echo 'you cant visit a api page without a true key';
 		}
 	}
-	public function add_exp_page($system){
+	/*public function add_exp_page($system){
 		if($this->app->add_exp($_GET['login_key'],$_GET['key'])){
 			$system->show_json(array('is_ok'=>1));
 		}else{
@@ -39,6 +27,8 @@ class api_control{
 		}
 	}*/
 	public function index_page($system){
+		header('Access-Control-Allow-Origin: http://blog.thestarweb.sweb');
+		header('Access-Control-Allow-Credentials:true');
 		//header("Content-type:text/javascript");
 		$login=new login_server($system);
 		$system->show_json(array('u'=>$login->get_now_user()));
