c8f770fb267811da0eff4ecdcd9c97545fd24c3c
thestarweb <597577939@qq.com>
2016-02-08 21:49:04 +0800

    修复ip记录的bug

--- sucs.sql
+++ sucs.sql
@@ -1,20 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.4.9
--- http://www.phpmyadmin.net
---
 -- Host: localhost
--- Generation Time: 2015-07-04 10:26:14
+-- Generation Time: 2016-02-08 13:30:15
 -- 服务器版本： 5.6.25
--- PHP Version: 5.6.10
+-- PHP Version: 7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `sucs`
@@ -26,6 +16,7 @@ SET time_zone = "+00:00";
 -- 表的结构 `admins`
 --
 
+DROP TABLE IF EXISTS `admins`;
 CREATE TABLE IF NOT EXISTS `admins` (
   `id` mediumint(8) unsigned NOT NULL,
   `uid` mediumint(8) unsigned NOT NULL,
@@ -33,12 +24,14 @@ CREATE TABLE IF NOT EXISTS `admins` (
   `type` tinyint(3) unsigned NOT NULL DEFAULT '1'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+
 -- --------------------------------------------------------
 
 --
 -- 表的结构 `admin_pages`
 --
 
+DROP TABLE IF EXISTS `admin_pages`;
 CREATE TABLE IF NOT EXISTS `admin_pages` (
   `id` smallint(5) unsigned NOT NULL,
   `pid` smallint(5) unsigned NOT NULL,
@@ -65,12 +58,14 @@ INSERT INTO `admin_pages` (`id`, `pid`, `title`, `is_menu`, `src`) VALUES
 -- 表的结构 `apps`
 --
 
+DROP TABLE IF EXISTS `apps`;
 CREATE TABLE IF NOT EXISTS `apps` (
   `aid` tinyint(4) NOT NULL,
   `name` varchar(20) NOT NULL DEFAULT '',
   `urlroot` varchar(100) NOT NULL DEFAULT '',
-  `key` char(11) NOT NULL DEFAULT '',
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `key` char(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 
 -- --------------------------------------------------------
 
@@ -78,6 +73,7 @@ CREATE TABLE IF NOT EXISTS `apps` (
 -- 表的结构 `forbidden_ip`
 --
 
+DROP TABLE IF EXISTS `forbidden_ip`;
 CREATE TABLE IF NOT EXISTS `forbidden_ip` (
   `id` tinyint(3) unsigned NOT NULL,
   `adder` mediumint(8) unsigned NOT NULL DEFAULT '0',
@@ -92,6 +88,7 @@ CREATE TABLE IF NOT EXISTS `forbidden_ip` (
 -- 表的结构 `friends`
 --
 
+DROP TABLE IF EXISTS `friends`;
 CREATE TABLE IF NOT EXISTS `friends` (
   `id` mediumint(8) unsigned NOT NULL,
   `uid` mediumint(8) unsigned NOT NULL,
@@ -105,6 +102,7 @@ CREATE TABLE IF NOT EXISTS `friends` (
 -- 表的结构 `group`
 --
 
+DROP TABLE IF EXISTS `group`;
 CREATE TABLE IF NOT EXISTS `group` (
   `gid` tinyint(3) unsigned NOT NULL,
   `gname` varchar(15) COLLATE utf8_bin NOT NULL DEFAULT '普通用户',
@@ -117,19 +115,29 @@ CREATE TABLE IF NOT EXISTS `group` (
   `get` smallint(3) NOT NULL DEFAULT '0' COMMENT '非vip组表示积分下限，vip组表示获得方式的id。'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- 转存表中的数据 `group`
+--
+
+INSERT INTO `group` (`gid`, `gname`, `read_rank`, `color`, `use_honor`, `max_signature`, `use_title`, `is_vip`, `get`) VALUES
+(0, '系统管理员', 255, '000000', 1, 2000, 1, 1, 0),
+(16, '普通用户', 10, '1', 0, 0, 0, 0, 0);
+
 -- --------------------------------------------------------
 
 --
 -- 表的结构 `logins`
 --
 
+DROP TABLE IF EXISTS `logins`;
 CREATE TABLE IF NOT EXISTS `logins` (
   `id` int(11) NOT NULL,
   `key` char(10) COLLATE utf8_bin NOT NULL DEFAULT '',
   `time` int(10) unsigned NOT NULL DEFAULT '0',
   `uid` mediumint(9) NOT NULL,
   `username` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '0',
-  `UA` varchar(500) COLLATE utf8_bin DEFAULT NULL
+  `UA` varchar(500) COLLATE utf8_bin DEFAULT NULL,
+  `ip` varchar(15) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -138,14 +146,16 @@ CREATE TABLE IF NOT EXISTS `logins` (
 -- 表的结构 `login_log`
 --
 
+DROP TABLE IF EXISTS `login_log`;
 CREATE TABLE IF NOT EXISTS `login_log` (
   `time` int(10) unsigned NOT NULL,
-  `ip` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `ip` varchar(15) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `uid` mediumint(9) DEFAULT NULL,
   `username` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `HTTP_USER_AGENT` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
   `is_true` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 
 -- --------------------------------------------------------
 
@@ -153,6 +163,7 @@ CREATE TABLE IF NOT EXISTS `login_log` (
 -- 表的结构 `login_remember`
 --
 
+DROP TABLE IF EXISTS `login_remember`;
 CREATE TABLE IF NOT EXISTS `login_remember` (
   `id` smallint(6) NOT NULL,
   `key` char(20) CHARACTER SET utf8 NOT NULL,
@@ -166,6 +177,7 @@ CREATE TABLE IF NOT EXISTS `login_remember` (
 -- 表的结构 `message`
 --
 
+DROP TABLE IF EXISTS `message`;
 CREATE TABLE IF NOT EXISTS `message` (
   `mid` int(10) unsigned NOT NULL,
   `sender` mediumint(8) unsigned NOT NULL DEFAULT '0',
@@ -175,17 +187,35 @@ CREATE TABLE IF NOT EXISTS `message` (
   `type` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+
 -- --------------------------------------------------------
 
 --
 -- 表的结构 `place`
 --
 
+DROP TABLE IF EXISTS `place`;
 CREATE TABLE IF NOT EXISTS `place` (
   `id` smallint(5) unsigned NOT NULL,
   `name` char(3) NOT NULL,
   `pid` smallint(5) unsigned NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `place`
+--
+
+INSERT INTO `place` (`id`, `name`, `pid`) VALUES
+(1, '中国', 0),
+(2, '美国', 0),
+(3, '加拿大', 0),
+(4, '英国', 0),
+(129, '湖南', 1),
+(128, '湖北', 1),
+(420, '长沙', 129),
+(421, '岳阳', 129),
+(422, '株洲', 129),
+(423, '湘潭', 129);
 
 -- --------------------------------------------------------
 
@@ -193,6 +223,7 @@ CREATE TABLE IF NOT EXISTS `place` (
 -- 表的结构 `reg_keys`
 --
 
+DROP TABLE IF EXISTS `reg_keys`;
 CREATE TABLE IF NOT EXISTS `reg_keys` (
   `key` varchar(100) NOT NULL,
   `end_time` int(10) unsigned NOT NULL DEFAULT '0',
@@ -201,16 +232,25 @@ CREATE TABLE IF NOT EXISTS `reg_keys` (
   `groups` varchar(500) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+
 -- --------------------------------------------------------
 
 --
 -- 表的结构 `system`
 --
 
+DROP TABLE IF EXISTS `system`;
 CREATE TABLE IF NOT EXISTS `system` (
   `key` varchar(20) NOT NULL DEFAULT '',
   `value` varchar(100) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- 转存表中的数据 `system`
+--
+
+INSERT INTO `system` (`key`, `value`) VALUES
+('opening', '1');
 
 -- --------------------------------------------------------
 
@@ -218,6 +258,7 @@ CREATE TABLE IF NOT EXISTS `system` (
 -- 表的结构 `uids`
 --
 
+DROP TABLE IF EXISTS `uids`;
 CREATE TABLE IF NOT EXISTS `uids` (
   `uid` mediumint(9) NOT NULL,
   `key` varchar(10) COLLATE utf8_bin DEFAULT NULL,
@@ -225,12 +266,14 @@ CREATE TABLE IF NOT EXISTS `uids` (
   `adder` smallint(6) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+
 -- --------------------------------------------------------
 
 --
 -- 表的结构 `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `uid` mediumint(8) unsigned NOT NULL,
   `username` varchar(10) COLLATE utf8_bin NOT NULL,
@@ -239,9 +282,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   `gid` tinyint(3) unsigned NOT NULL DEFAULT '16',
   `email` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   `reg_time` int(10) unsigned NOT NULL DEFAULT '0',
-  `reg_ip` varchar(12) COLLATE utf8_bin NOT NULL DEFAULT '127.0.0.1',
+  `reg_ip` varchar(15) COLLATE utf8_bin NOT NULL DEFAULT '127.0.0.1',
   `last_login_time` int(10) unsigned DEFAULT NULL,
-  `last_login_ip` varchar(12) COLLATE utf8_bin DEFAULT NULL,
+  `last_login_ip` varchar(15) COLLATE utf8_bin DEFAULT NULL,
   `sex` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `signature` varchar(2000) COLLATE utf8_bin DEFAULT NULL,
   `title` varchar(20) COLLATE utf8_bin DEFAULT NULL,
@@ -250,12 +293,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `web` varchar(50) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- 转存表中的数据 `users`
+--
+
+
 -- --------------------------------------------------------
 
 --
 -- 表的结构 `user_exc`
 --
 
+DROP TABLE IF EXISTS `user_exc`;
 CREATE TABLE IF NOT EXISTS `user_exc` (
   `uid` mediumint(8) unsigned NOT NULL,
   `经验` int(11) NOT NULL DEFAULT '0',
@@ -264,6 +313,7 @@ CREATE TABLE IF NOT EXISTS `user_exc` (
   `元宝` int(11) NOT NULL DEFAULT '0',
   `贡献` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 
 --
 -- Indexes for dumped tables
@@ -431,6 +481,3 @@ ALTER TABLE `users`
 --
 ALTER TABLE `user_exc`
   MODIFY `uid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
