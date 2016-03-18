6bb62097c22add6cb3a7f6c2643b89f6459e9c81
thestarweb <597577939@qq.com>
2016-03-18 21:57:51 +0800

    test

--- control/api_control.php
+++ control/api_control.php
@@ -12,6 +12,22 @@ class api_control{
 			echo 'you cant visit a api page without a true key';
 		}
 	}
+	public function message_page($system){
+		/*$this->app->is_admit();
+		set_time_limit(0);
+header(”Connection: Keep-Alive”);
+header(”Proxy-Connection: Keep-Alive”);
+		$_SESSION['test']=isset($_SESSION['test'])?($_SESSION['test']+1):1;
+		for($i=0;$i<5000;$i++){
+			echo ' ';
+		}print str_repeat(" ", 40960);
+		while (true) {
+			sleep(5);
+			echo rand(10,50).$_SESSION['test'].'<br/>';
+			ob_flush();
+			flush();
+		}*/
+	}
 	/*public function add_exp_page($system){
 		if($this->app->add_exp($_GET['login_key'],$_GET['key'])){
 			$system->show_json(array('is_ok'=>1));
@@ -27,10 +43,8 @@ class api_control{
 		}
 	}*/
 	public function index_page($system){
-		header('Access-Control-Allow-Origin: http://blog.thestarweb.sweb');
-		header('Access-Control-Allow-Credentials:true');
-		//header("Content-type:text/javascript");
-		$login=new login_server($system);
-		$system->show_json(array('u'=>$login->get_now_user()));
 	}
+	/*public function __destruct(){
+		$_SESSION['test']-=1;
+	}*/
 }
