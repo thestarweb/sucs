f915a84c38a0fdd03913edc845021f51e0d18f45
thestarweb <597577939@qq.com>
2016-04-27 22:03:05 +0800

    命名调整

--- servers/shop_server.php
+++ servers/shop_server.php
@@ -1,5 +1,5 @@
 <?php
-class market_server{
+class shop_server{
 	public function __construct($system){
 		$this->system=$system;
 	}
@@ -7,7 +7,7 @@ class market_server{
 	public function get_public_list(){
 		return $this->system->db()->do_SQL('SELECT `gid`,`name`,`jiesao`,`take_type`,`take_number` FROM `'.self::table.'` WHERE `need_type`=0');
 	}
-	public function good_info($id){
+	public function goods_info($id){
 		$id+=0;
 		$res=$this->system->db()->do_SQL('SELECT `name`,`jiesao`,`take_type`,`take_number` FROM `'.self::table.'` WHERE `gid`='.$id);
 		return $res?$res[0]:array('name'=>'','jiesao'=>'','take_type'=>'','take_number'=>'');
