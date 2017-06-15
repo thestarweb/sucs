d0366e3021b8867b63c9071a1af7c01ebf623006
thestarweb <597577939@qq.com>
2017-06-15 16:08:14 +0800

    验证码增强

--- img/addition.php
+++ img/addition.php
@@ -1,41 +1,54 @@
 <?php
-/*  100以内加减法验证码图片版v1.0.1.0002  */
+/*  100以内加减法验证码图片版v2.0.0.0006  */
 /*     版权归星星站点所有，保留所有权利    */
-/*                by 星星              */
-/*严禁任何人对本代码进行任何*/
+/*                             by 星星                          */
+/*                 转载请保留以上信息                 */
 	header('Cache-Control: no-cache, must-revalidate');//禁用缓存
 	session_start();//为后面将数据保存到session做准备
-	$name=@$_GET['name']?$_GET['name']:'addition';
+	$name=isset($_GET['name'])?$_GET['name']:'addition';//存放名称
+
 	$number1=rand(30,70);//生成第一个数字（伪随机数）
 	$number2=rand(0,30);//生产第二个数字
 	$operator=rand(0,1);//生成运算符，0表示+，1表示-
+
 	$image=imagecreatetruecolor(50,20);//创建画布（背景为黑色）
+	//定义随机颜色
+	$color=[
+		imagecolorallocate($image,255,0,0),
+		imagecolorallocate($image,0,255,0),
+		imagecolorallocate($image,255,255,0),
+		imagecolorallocate($image,20,208,255),
+		imagecolorallocate($image,150,150,255),
+		imagecolorallocate($image,255,150,50),
+		imagecolorallocate($image,255,255,255),
+		imagecolorallocate($image,255,150,150),
+		imagecolorallocate($image,200,200,200)
+	];
+	$max_color_num=count($color)-1;
+
 	//通过for循环创建干扰机器人的线条，这里生成十条
-	for($i=0;$i<10;$i++){
-		//随机生成一个颜色（这里生成的颜色比较暗，以便于用户可以看清，而机器人无法看清
-		$color[0]=imagecolorallocate($image,rand(0,200),rand(0,200),rand(0,200));
+	for($i=0;$i<20;$i++){
+		$startx=rand(0,50);
+		$starty=rand(0,20);
 		//使用随机颜色在画布的随机位置绘制直线
-		imageline($image,rand(0,50),rand(0,20),rand(0,50),rand(0,20),$color[0]);
+		imageline($image,$startx,$starty,$startx+rand(-4,4),$starty+rand(-2,2),$color[rand(0,$max_color_num)]);
 	}
-	//下面是用于给算式的随机颜色（较亮）
-	$color[1]=imagecolorallocate($image,255,0,0);
-	$color[2]=imagecolorallocate($image,0,255,0);
-	$color[3]=imagecolorallocate($image,150,150,255);
-	$color[4]=imagecolorallocate($image,200,200,200);
+
 	//将第一个数绘制到画布
-	imagestring($image,5,2,1,$number1,$color[rand(1,4)]);
+	imagettftext($image,rand(14,16),rand(-5,5),rand(-2,0),rand(16,19),$color[rand(0,$max_color_num)],'arial.ttf',$number1);
 	//判断运算符
 	if($operator==0){
 		//加法
 		$_SESSION[$name]=$number1+$number2;//将结果保存至session
-		imagestring($image,5,20,1,"+",$color[rand(1,4)]);//在画布上绘制运算符
+		imagettftext($image,rand(16,18),rand(-5,5),rand(18,20),rand(16,19),$color[rand(0,$max_color_num)],'arial.ttf','+');
 	}else{
 		//减法（详细同上）
 		$_SESSION[$name]=$number1-$number2;
-		imagestring($image,5,20,1,"-",$color[rand(1,4)]);
+		imagettftext($image,rand(24,25),rand(-5,5),rand(18,20),rand(16,19),$color[rand(0,$max_color_num)],'arial.ttf','-');
 	}
 	//绘制第二个数
-	imagestring($image,5,28,1,$number2,$color[rand(1,4)]);
+	imagettftext($image,rand(14,16),rand(-5,5),rand(28,30),rand(16,19),$color[rand(0,$max_color_num)],'arial.ttf',$number2);
+
 	header("Content-type:image/png");//这句话用于修改http协议的头部，让浏览器知道这是一张图片
 	imagepng($image);//将图片输出
 	imagedestroy($image);//销毁服务器端的图片，释放内存
