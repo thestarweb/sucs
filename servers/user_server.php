d21622cdadec9021dc462f38ff51d6619f6d7d8e
thestarweb <597577939@qq.com>
2017-09-18 19:22:13 +0800

    国际化扫尾基本完成

--- servers/user_server.php
+++ servers/user_server.php
@@ -106,7 +106,6 @@
 		public function add_user_for_key($username,$password,$uid,$key){
 			$db=$this->system->db();
 			$gid=$db->u_exec('SELECT `group` FROM `@%_uids` WHERE `uid`=? AND `key`=?',array($uid,$key));
-			//var_dump($gid);exit;
 			if(array_key_exists(0,$gid)){
 				return $this->add_user($username,$password,$uid,$gid[0]['group']);
 			}else{
